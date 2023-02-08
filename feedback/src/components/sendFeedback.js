c411b89edf9113446a40606006037f6d914250d2
Addie Nelson <anelso12@travelers.com>
2023-02-08 14:23:23 -0500

    sendFeedback attempt at template for fetch post

--- feedback/src/components/sendFeedback.js
+++ feedback/src/components/sendFeedback.js
@@ -1,18 +1,34 @@
 import React, {useEffect, useState} from 'react'
 
-function SendFeedback(){
+function SendFeedback({empId, mgrId}){
     const {message, setMessage}=useState("")
     const messageHandler=(event)=>{
         setMessage(event.target.value)
     }
-    function sendFeedback(){
-        
+    async function sendFeedback(){
+        const feedback={
+            "text":message,
+            "employeeID":+empId,
+            "managerID":+mgrId
+        }
+        let url="http://localhost:/"//server route for posting to managerfeedback collection
+        try{
+            const postMessage = await fetch(url,{
+                method:'post',
+                headers: {'Content-Type': 'application/json'},
+                body:JSON.stringify(feedback)
+            })
+            console.log()
+        }catch{
+            console.log("Could not post feedback.")
+        }
     }
     return(
         <>
         <h3>Enter Feedback</h3>
         <form onSubmit={sendFeedback} >
         <input type={"text"} value={message} onChange={messageHandler} />
+        <button type={"submit"}>Send</button>
         </form>
         </>
     )

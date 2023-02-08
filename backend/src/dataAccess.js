636b9c89b5641d184e6e6e903a9968911a4e83bd
Reid Hopkins <rhopkin5@travelers.com>
2023-02-08 14:23:57 -0500

    Removed backend node_modules

--- backend/src/dataAccess.js
+++ backend/src/dataAccess.js
@@ -17,15 +17,14 @@ module.exports.call = async function call(operation, parameters, callback) {
 
     //Execute Operations
     // available operations: 
-    // ['initfeedback'|'clearfeedbacks'|'findallfeedbacks'|'findfeedback'|'updatefeedback' ]
+    // ['initfeedback'|'clearfeedbacks'|'findallfeedbacks'|'findmanagerfeedback'|'findemployeefeedback' ]
+    let feedbacks;
     switch (operation.toLowerCase()) {
         case 'initfeedbacks':
             const initialRecords = [
-                // { "_id": "5dfb7b07cef0eaea7dedefc6", "isbn": "001", "price": 18, "title": "Moby Dick" },
-                // { "_id": "5dfb7b3ccef0eaea7dedefd5", "isbn": "002", "price": 4.48, "title": "War and Peace" }
-                { "_id" : ObjectId("63e3bf526deda6a1b3a67115"), "text" : "asdf asdf asdf", "employeeID" : 3, "managerID" : 4 },
-                { "_id" : ObjectId("63e3bf526deda6a1b3a67116"), "text" : "asdf asdf asdf", "employeeID" : 1, "managerID" : 2 },
-                { "_id" : ObjectId("63e3bf526deda6a1b3a67117"), "text" : "asdf asdf asdf", "employeeID" : 5, "managerID" : 6 }
+                {"_id": "63e3bf526deda6a1b3a67115", "text": "asdf asdf asdf", "employeeID": 3, "managerID": 4},
+                {"_id": "63e3bf526deda6a1b3a67116", "text": "asdf asdf asdf", "employeeID": 1, "managerID": 2},
+                {"_id": "63e3bf526deda6a1b3a67117", "text": "asdf asdf asdf", "employeeID": 5, "managerID": 6}
             ];
             await collection.insertMany(initialRecords).then(
                 (result)=>{ callback({ status: "feedback records have been initialized." })},
@@ -41,21 +40,18 @@ module.exports.call = async function call(operation, parameters, callback) {
             break;
 
         case 'findallfeedbacks':
-            const feedbacks = await collection.find({}).toArray();
+            feedbacks = await collection.find({}).toArray();
             callback({ feedbacks: feedbacks });
             break;
 
-        case 'findfeedback':
-            const feedback = await collection.findOne({ isbn: parameters.isbn });
-            callback({ feedback:feedback });
+        case 'findmanagerfeedback':
+            feedbacks = await collection.find({ managerID: parameters.managerID });
+            callback({ feedbacks: feedbacks });
             break;
 
-        case 'updatefeedback':
-            await collection.updateOne(
-                { isbn: parameters.feedback.isbn },
-                {$set: parameters.feedback},
-                {upsert: true});
-            callback({ status: 'item updated:'+parameters.feedback.isbn });
+        case 'findemployeefeedback':
+            feedbacks = await collection.find({ employeeID: parameters.employeeID });
+            callback({ feedbacks: feedbacks });
             break;
 
         default:

398cbd74f7b2fab9a0c33a5315f441dd8e14631b
Reid Hopkins <rhopkin5@travelers.com>
2023-02-08 10:34:53 -0500

    Added starting App.js routing

--- feedback/src/App.js
+++ feedback/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, BrowserRouter, } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <Route path="/">
+          {/* login page here */}
+        </Route>
+      </BrowserRouter>
+      <div className="App">
+        <h1>Login page</h1>
+      </div>
+    </React.Fragment>
   );
 }
 

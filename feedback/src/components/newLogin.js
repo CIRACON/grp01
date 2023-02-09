c26d070a41b43728bb7382ca093cb3c043ac865b
Addie Nelson <anelso12@travelers.com>
2023-02-09 15:41:55 -0500

    newUser functional

--- feedback/src/components/newLogin.js
+++ feedback/src/components/newLogin.js
@@ -2,50 +2,53 @@ import React, {useState, useEffect} from 'react'
 
 function NewLogin(){
     const [password, setPassword] = useState("")
-    const [empId, setEmpId]  = useState("")
+    const [empId, setEmpId]  = useState()
     const [mgrId, setMgrId]  = useState("")
-    const pChangeHandler = (event) =>{
-        setPassword(event.target.value)
-    }
-    const mgrIdHandler = (event) =>{
-        setMgrId(event.target.value)
-    }
+    const pChangeHandler = (event) =>{setPassword(event.target.value)}
+    const mgrIdHandler = (event) =>{setMgrId(event.target.value)}
     async function createUser(){
         const postUrl= 'http://localhost:3001/newemployee'
         const getUrl = 'http://localhost:3001/employees'
+        let newEmp;
         try{
             let allEmps = await fetch(getUrl)
-                //.then(res=>res.json())
-                .then(res=>res.length)
-            setEmpId(allEmps+1)
+                .then(res=>res.json())
+                console.log(allEmps.length)
+                const maxID=Math.max(...allEmps.map(e=>e.id))
+                console.log({maxID})
+                setEmpId(maxID+1)
+        
+        newEmp={
+            "id":maxID+1,
+            "managerID":+mgrId
+        }
         }catch{
-            console.log("Could not get number of employees")
+            console.log("Could not get employees")
         }
-        const newEmp={
-            "id":empId,
-            "managerId":mgrId
-        }
+        console.log(newEmp)
         try{
             await fetch(postUrl,{
                 method:'post',
                 headers: {'Content-Type': 'application/json'},
                 body:JSON.stringify(newEmp)
             })
-            .then(window.location = `/employee/${empId}`)
+            //window.location = `/employee/${empId}`)
         } catch{
-            console.log("Could not enter new employee")
+            console.log("Could not enter new employee.")
         }
 
     }
     return(
         <>
         <h1>Create user profile</h1>
-        <form onSubmit={createUser}>
+        <form onSubmit={(e)=>{e.preventDefault();createUser()}}>
         <label>Password</label><br/>
         <input type={"text"} value={password} onChange={pChangeHandler}/>
         <br/>
         <label>Manager's ID</label><br/>
         <input type={"text"} value={mgrId} onChange={mgrIdHandler}/>
+        <br/>
+        <button type={"submit"}>Create user</button>
         </form>
         </>
     )
